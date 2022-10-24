cef1895b12a2856db98ae65c0cf8ba0785848339
andrijdemianczuk <andrijdemianczuk>
2022-10-24 22:03:14 +0000

    fixed functions calling dictionary types

--- Testing/API-Nested-Objects-Test.py
+++ Testing/API-Nested-Objects-Test.py
@@ -4,8 +4,14 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC This is a __*development*__ notebook - in order to 'production-ize' this code we'll need to remove un-necessary functions (mostly displays, schema prints etc.)
+
+# COMMAND ----------
+
 import requests
 
+#In reality, we would probably have a list of zip codes to iterate through. Might tackle this later if time permits. Should be pretty easy to build an iterator.
 #url = "https://vanitysoft-boundaries-io-v1.p.rapidapi.com/rest/v1/public/carrierRoute/zipcode/20019"
 url = "https://vanitysoft-boundaries-io-v1.p.rapidapi.com/rest/v1/public/carrierRoute/zipcode/98122"
 
@@ -124,14 +130,60 @@ display(properties_df)
 
 # COMMAND ----------
 
-#Create reference pointers to both dataframes. Tuples are immutable and more efficient than lists - use them if you're not modifying your collections further.
-df_tuple = (coordinates_df, properties_df)
+#Create reference pointers to both dataframes. Tuples are immutable and more efficient than lists - use them if you're not modifying your collections further. Remember that the tuple is what's immutable, NOT the referenced object(s) - they can still be changed if need-be
+# df_tuple = (coordinates_df, properties_df)
 
 # COMMAND ----------
 
-for i in df_tuple:
-  i.printSchema()
+# MAGIC %md
+# MAGIC **!!!!Below is still in dev**
 
 # COMMAND ----------
 
+def init_props(type) -> dict:
+  return {
+    "delta_loc": f"dbfs:/FileStore/Users/andrij.demianczuk@databricks.com/tmp/CarrierRoutes/data/{type}",
+    "write_fmt": 'delta',
+    "table_name": f'b_carrier_{type}',
+    "write_mode": 'overwrite',
+    "type": type,
+    "database": "ademianczuk"
+  }
 
+# COMMAND ----------
+
+#DEBUG ONLY
+# spark.sql(f"DROP TABLE IF EXISTS {database}.{table_name}")
+# spark.sql("CREATE TABLE " + database + "."+ table_name + " USING DELTA LOCATION '" + delta_loc + "'")
+
+# COMMAND ----------
+
+def update_delta_fs(df, params):
+  df.write \
+  .format(params["write_fmt"]) \
+  .mode(params["write_mode"]) \
+  .save(params["delta_loc"])
+
+# COMMAND ----------
+
+def create_delta_table(df):
+  
+  if spark._jsparkSession.catalog().tableExists(database, table_name):
+    
+    print("table already exists..... appending data")
+    df.createOrReplaceTempView(f"vw_{ttype}")  
+    
+    spark.sql(f"MERGE INTO {database}.{table_name} USING vw_{ttype} \
+      ON vw_{ttype}.h3-index = {database}.{table_name}.h3-index \
+      WHEN MATCHED THEN UPDATE SET * WHEN NOT MATCHED THEN INSERT *")
+  
+  else:
+    print("table does not exist..... creating a new one")
+    
+    # Write the data to its target.
+    spark.sql("CREATE TABLE " + database + "."+ table_name + " USING DELTA LOCATION '" + delta_loc + "'")
+
+# COMMAND ----------
+
+update_delta_fs(coordinates_df, init_props("coordinates"))
+update_delta_fs(properties_df, init_props("properties"))
